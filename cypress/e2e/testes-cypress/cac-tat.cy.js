be3c075a7d02648a9c16d34e47a686b56d0e5e4a
Raul Mendes <raul@qcx.com.br>
2024-01-19 12:17:25 -0300

    End of course - Basic

--- cypress/e2e/testes-cypress/cac-tat.cy.js
+++ cypress/e2e/testes-cypress/cac-tat.cy.js
@@ -179,4 +179,26 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it('acessa a página da política de privacidade removendo o target e então clicando no link', function(){
       cy.get('#privacy a').invoke('removeAttr', 'target').click()
     })
+
+    it('preenche a area de texto usando o comando invoke', function() {
+      cy.get('#open-text-area')
+        .invoke('val', lorem)
+        .should('have.value', lorem)
+    })
+
+    it('faz uma requisição HTTP', function() {
+      cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+      .should(function(response) {
+        const { status, statusText, body } = response
+        expect(status).to.equal(200)
+        expect(statusText).to.equal('OK')
+        expect(body).to.include('CAC TAT')
+      })
+    })
+
+    it.only('encontre o gato', function() {
+      cy.get('#cat')
+        .invoke('show')
+        .should('be.visible')
+    })
 })
